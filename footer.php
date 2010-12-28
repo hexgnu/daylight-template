4f2227b4290aefe70308df1262c20a7a9d7b33eb
Matt Kirk <mattk@crayon.corp.local>
2010-12-27 16:57:03 -0800

    Changing up the theme a bit

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div id="footer">
+	<div class="footContent">
+	  <p>Daylight Masons Lodge #232 &copy;<?php echo date("Y"); ?></p>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php bloginfo('name'); ?> <?php wp_title('-'); ?></title>
 <?php wp_head();?>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css"  />
 </head>
 <body>
   <?php get_header(); ?>
@@ -14,41 +15,45 @@
 
   </div>
   <div id="Second_Column">
+    <?php if (have_posts()) {
+      while (have_posts()) {
+        the_post();
+    ?>
+    <h2>
+    <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
+    <?php the_title(); ?></a></h2>
+    <small><?php the_time('F jS, Y') ?> by <?php the_author() ?> </small>
+    <?php  
+      }
+    }?>
+  <!-- <h1>Welcome to Daylight Masonic Lodge</h1>
+    <hr />
+    <a href="http://maps.yahoo.com/map?q1=307%20Ne%20Maple%20Leaf%20Pl%20Seattle%20WA%2098115-5315%20us&amp;mag=9&amp;ard=1#mvt=m&amp;lat=47.680662&amp;lon=-122.325912&amp;mag=9&amp;zoom=14&amp;q1=307%20Ne%20Maple%20Leaf%20Pl%20Seattle%20WA%2098115-5315%20us&amp;gid1=22142217" target="_blank"><img src="images/map.jpg" width="185" height="185" align="right" alt="Map/Directions" border="0" style="padding:5px;" /></a>
+    <p>Daylight lodge is a Masonic lodge located in Seattle. Our lodge has a unique heritage related to the arts and of providing Masonry to those who either work evenings or prefer to attend lodge on weekend mornings. Our members come from a wide range of backgrounds and interests. Good fellowship, good victuals and good drinks are a fundamental nature of the lodge.</p>
+    <br />
 
-  <h1>Welcome to Daylight Masonic Lodge</h1>
-  <hr />
-  <a href="http://maps.yahoo.com/map?q1=307%20Ne%20Maple%20Leaf%20Pl%20Seattle%20WA%2098115-5315%20us&amp;mag=9&amp;ard=1#mvt=m&amp;lat=47.680662&amp;lon=-122.325912&amp;mag=9&amp;zoom=14&amp;q1=307%20Ne%20Maple%20Leaf%20Pl%20Seattle%20WA%2098115-5315%20us&amp;gid1=22142217" target="_blank"><img src="images/map.jpg" width="185" height="185" align="right" alt="Map/Directions" border="0" style="padding:5px;" /></a>
-  <p>Daylight lodge is a Masonic lodge located in Seattle. Our lodge has a unique heritage related to the arts and of providing Masonry to those who either work evenings or prefer to attend lodge on weekend mornings. Our members come from a wide range of backgrounds and interests. Good fellowship, good victuals and good drinks are a fundamental nature of the lodge.</p>
-  <br />
+    <p>Founded in 1920 predominantly by entertainers, Daylight follows the objectives of the Masonic philosophy which includes the study of the liberal arts and sciences and the practice of brotherly love, relief and truth.</p>
+    <br />
+    <p>Our lodge sponsors many community activities including:</p>
+    <br />
+    <ul>
+      <li>Fremont Solstice Festival Bandstand</li>
+      <li><a href="http://daylight232.web.officelive.com/SYSO2010.html" title="Youth Symphony Annual Dinner" target="_blank">Seattle Youth Symphony Annual Dinner</a></li>
+      <li>Many More Events! </li>
+    </ul>
+    <br />
+    <p>Lodge meetings are conducted on the 3rd Saturday of each month at Seattle's Green Lake Masonic Hall. Meetings start at 10:00 a.m. and lunch is served at noon. No one ever goes hungry!</p>
+    <br />
 
-  <p>Founded in 1920 predominantly by entertainers, Daylight follows the objectives of the Masonic philosophy which includes the study of the liberal arts and sciences and the practice of brotherly love, relief and truth.</p>
-  <br />
-  <p>Our lodge sponsors many community activities including:</p>
-  <br />
-  <ul>
-  	<li>Fremont Solstice Festival Bandstand</li>
-  	<li><a href="http://daylight232.web.officelive.com/SYSO2010.html" title="Youth Symphony Annual Dinner" target="_blank">Seattle Youth Symphony Annual Dinner</a></li>
-  	<li>Many More Events! </li>
-  </ul>
-  <br />
-  <p>Lodge meetings are conducted on the 3rd Saturday of each month at Seattle's Green Lake Masonic Hall. Meetings start at 10:00 a.m. and lunch is served at noon. No one ever goes hungry!</p>
-  <br />
+    <p>The Green Lake Masonic Hall is located in the Green Lake business district at 307 NE Maple Leaf Place.</p>
+    <br />
 
-  <p>The Green Lake Masonic Hall is located in the Green Lake business district at 307 NE Maple Leaf Place.</p>
-  <br />
+    <p>Daylight Lodge is one with fresh ideas, younger officers, innovative programs and community involvement. We welcome new members!</p>
 
-  <p>Daylight Lodge is one with fresh ideas, younger officers, innovative programs and community involvement. We welcome new members!</p>
-
-  </div>
-  <div class="clearall"></div>
+    </div>
+    <div class="clearall"></div> -->
   </div>
   </div><!-- container close -->
-  <div id="footer">
-
-  	<div class="footContent">
-  	  <p>Footer Content | Web Design by : UrbanAnalog - Copyright stuff goes here 2010</p></div>
-
-  </div>
-
+  <?php get_footer();?>
 </body>
 </html>
