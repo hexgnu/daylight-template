56ce199803e2dd9f42c3786042100e1190af39ab
Matt Kirk <mattk@crayon.corp.local>
2010-12-27 16:39:56 -0800

    Changing around to have index.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<div id="utilityNav">
+	<div id="utilContent">
+		<p class="left"><a href="#">Lodge Merchandise</a>, and <a href="#">Pay Dues Online</a>!</p>
+		<p class="right">Daylight Lodge No. 232 F.&amp; A.M. | t: 206.632.2970 | Seattle, WA.</p>
+    </div>
+</div>
+<div id="container">
+<div id="header">
+  <h1>&nbsp;</h1></div><!-- head close -->
+
+<div id="navigation">
+	<a href="#"><span>Home</span></a>
+	<a href="#"><span>About</span></a>
+	<a href="#"><span>Join</span></a>
+	<a href="#"><span>Events</span></a>
+	<a href="#"><span>Officers</span></a>
+	<a href="#"><span>Newsletters</span></a>
+	<a href="#"><span>Masonic Art</span></a>
+</div>
