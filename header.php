f66dfaf2c601c626739221f96323aaa7b33f72b6
Matt Kirk <matt@matthewkirk.com>
2011-01-02 13:54:50 -0800

    Making changes for daylight-template

--- header.php
+++ header.php
@@ -1,19 +1,10 @@
-<div id="utilityNav">
-	<div id="utilContent">
-		<p class="left"><a href="#">Lodge Merchandise</a>, and <a href="#">Pay Dues Online</a>!</p>
-		<p class="right">Daylight Lodge No. 232 F.&amp; A.M. | t: 206.632.2970 | Seattle, WA.</p>
-    </div>
-</div>
 <div id="container">
+	<div id="utilityNav">
+		<div id="utilContent">
+			<p class="left"><a href="<?php echo get_permalink(1); ?>">Lodge Merchandise</a>, and <a href="<?php echo get_permalink(1); ?>">Pay Dues Online</a>!</p>
+			<p class="right">Daylight Lodge No. 232 F.&amp; A.M. | t: 206.632.2970 | Seattle, WA.</p>
+	  </div>
+	</div>
 <div id="header">
   <h1>&nbsp;</h1></div><!-- head close -->
-
-<div id="navigation">
-	<a href="#"><span>Home</span></a>
-	<a href="#"><span>About</span></a>
-	<a href="#"><span>Join</span></a>
-	<a href="#"><span>Events</span></a>
-	<a href="#"><span>Officers</span></a>
-	<a href="#"><span>Newsletters</span></a>
-	<a href="#"><span>Masonic Art</span></a>
-</div>
+<?php wp_nav_menu(array('link_before' => '<span>', 'link_after' => '</span>', 'container_id' => 'navigation')); ?>
